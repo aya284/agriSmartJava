04c82d1dd505a6cb7fb766e5c2ccba177cc84881
aya284 <ayafdhila@gmail.com>
2026-04-14 20:44:15 +0200

    pull updates

--- src/main/java/main/MainApp.java
+++ src/main/java/main/MainApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import utils.MyConnection;
 
@@ -16,6 +17,9 @@ public class MainApp extends Application {
         Scene scene = new Scene(root, 1200, 750);
         scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
         primaryStage.setTitle("AgriSmart Desktop");
+        if (getClass().getResource("/images/logo.png") != null) {
+            primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/logo.png")));
+        }
         primaryStage.setScene(scene);
         primaryStage.setMinWidth(1000);
         primaryStage.setMinHeight(650);
